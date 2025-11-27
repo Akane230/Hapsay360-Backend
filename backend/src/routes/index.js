ba0f1f983bd46e6732f1c8bbb568988307feaf96
John Dave Rojo <rojo.dave2004@gmail.com>
2025-11-27 21:54:32 +0800

    unified user and admin database

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,18 +1,19 @@
-import express from 'express';
-import authRoutes from './auth.routes.js';
-import userRoutes from './users.routes.js';
-import clearanceRoutes from './clearance.routes.js';
-import policeStationRoutes from './policeStation.routes.js';
-import blotterRoutes from './blotter.routes.js';
+import express from "express";
+import authRoutes from "./auth.routes.js";
+import userRoutes from "./users.routes.js";
+import clearanceRoutes from "./clearance.routes.js";
+import policeStationRoutes from "./policeStation.routes.js";
+import blotterRoutes from "./blotter.routes.js";
+import applicationRoutes from "./application.routes.js";
 
 const router = express.Router();
 
 // Mount all routes
-router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
-router.use('/clearance', clearanceRoutes)
-router.use('/police-stations', policeStationRoutes);
-router.use('/blotters', blotterRoutes);
+router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+router.use("/clearance", clearanceRoutes);
+router.use("/police-stations", policeStationRoutes);
+router.use("/blotters", blotterRoutes);
+router.use("/application", applicationRoutes);
 
 export default router;
-
